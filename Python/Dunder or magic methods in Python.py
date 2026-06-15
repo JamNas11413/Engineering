b3f8c066f86d288cf9e4ba59b23e330e3d5e6f84
JamNas11413 <jamalnasarstd@gmail.com>
2026-06-15 08:15:44 +0500

    modifyingit

--- Python/Dunder or magic methods in Python.py
+++ Python/Dunder or magic methods in Python.py
@@ -1,9 +1,9 @@
 # Dunder or magic methods in Python 
-    # Python magic (dunder) methods are special methods with double underscores __ that enable operator overloading and custom object behavior.
+    # Python magic (dunder) methods are special methods with double underscores __ that enable operator overloading and custom object behavior.   
+                   # Operator overloading in Python allows you to redefine how built-in operators (like +, -, *, ==, and <) behave when used with your custom objects. Python implements this behavior through special methods, commonly referred to as dunder methods (double underscore methods) or magic methods. When an operator is used on an object, Python automatically translates that operator into its corresponding dunder method call under the hood. For example, when you use the + operator on two objects, Python internally calls the __add__ method of the first object, passing the second object as an argument. By defining these dunder methods in your class, you can customize how operators work with instances of your class, allowing for more intuitive and flexible code.
+                   # custom object behavior refers to the ability to define how objects of a class should behave in certain situations, such as when they are printed, compared, or used in arithmetic operations. By implementing specific dunder methods in your class, you can control how instances of that class respond to various operations and interactions, making your objects more versatile and easier to work with in different contexts.
 
     # The below code displays the magic methods inherited by int class.
-
-from sqlalchemy import Numeric
 
 
 print(dir(int))
@@ -12,6 +12,7 @@ print(dir(int))
 # Output
 
 # ['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '_...
+
 
 # Explanation: dir(int) lists all attributes and methods of the int class, including magic (dunder) methods like __add__, __str__, etc.
 
@@ -24,6 +25,8 @@ print(dir(int))
 
 # Output
     # ['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '_...
+
+
 
 # Magic methods in Python
     # Below are the lists of Python magic methods and their uses.
@@ -40,28 +43,25 @@ print(dir(int))
             # __pos__(self): Implements behavior for unary positive 
 
         # 3. Arithmetic operators
+            # __add__(self, other): Implements behavior for the + operator (addition).
+            # __sub__(self, other): Implements behavior for the - operator (subtraction).
+            # __mul__(self, other): Implements behavior for the * operator (multiplication).
+            # __truediv__(self, other): Implements behavior for the / operator (true division).
+            # __mod__(self, other): Implements behavior for the % operator (modulus).
 
-        # __add__(self, other): Implements behavior for the + operator (addition).
-        # __sub__(self, other): Implements behavior for the - operator (subtraction).
-        # __mul__(self, other): Implements behavior for the * operator (multiplication).
-        # __truediv__(self, other): Implements behavior for the / operator (true division).
-        # __mod__(self, other): Implements behavior for the % operator (modulus).
+        # 4. String Magic Methods
+            # __str__(self): Defines behavior for when str() is called on an instance of your class.
+            # __repr__(self): To get called by built-int repr() method to return a machine readable representation of a type.
+            # __hash__(self): It has to return an integer, and its result is used for quick key comparison in dictionaries.
+            # __bool__(self): Returns True or False to determine the truth value of an object, used in conditions and bool() calls.
+            # __dir__(self): This method to return a list of attributes of a class.
 
-    # 4. String Magic Methods
-
-        # __str__(self): Defines behavior for when str() is called on an instance of your class.
-        # __repr__(self): To get called by built-int repr() method to return a machine readable representation of a type.
-        # __hash__(self): It has to return an integer, and its result is used for quick key comparison in dictionaries.
-        # __bool__(self): Returns True or False to determine the truth value of an object, used in conditions and bool() calls.
-        # __dir__(self): This method to return a list of attributes of a class.
-
-    # 5. Comparison magic methods
-
-        # __eq__(self, other): Defines behavior for the equality operator, ==.
-        # __ne__(self, other): Defines behavior for the inequality operator, !=.
-        # __lt__(self, other): Defines behavior for the less-than operator, <.
-        # __gt__(self, other): Defines behavior for the greater-than operator, >.
-        # __ge__(self, other): Defines behavior for the greater-than-or-equal-to operator, >=.
+        # 5. Comparison magic methods
+            # __eq__(self, other): Defines behavior for the equality operator, ==.
+            # __ne__(self, other): Defines behavior for the inequality operator, !=.
+            # __lt__(self, other): Defines behavior for the less-than operator, <.
+            # __gt__(self, other): Defines behavior for the greater-than operator, >.
+            # __ge__(self, other): Defines behavior for the greater-than-or-equal-to operator, >=.
 
     # Examples of Magic Methods
     # 1. __init__ Method
@@ -106,7 +106,6 @@ if __name__ == '__main__':
 
 
 # Output
-
 # Object: Hello
 
 # Explanation:
